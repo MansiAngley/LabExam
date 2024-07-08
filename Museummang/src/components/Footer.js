45388acb63759b4b167b9b8a5070f9960c7353f6
Mansi Angley <98343806+MansiAngley@users.noreply.github.com>
2024-07-08 12:46:22 +0530

    Add files via upload

---
+++ Museummang/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <>
+      <div className="footer text-center">
+        <h3>Footer Part</h3>
+      </div>
+    </>
+  )
+}
